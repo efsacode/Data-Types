c1941b00fb15c4957f5770bf51665f34345da3f0
efsa <113553752+efsacode@users.noreply.github.com>
2022-09-15 14:56:16 +0300

    Data-Types

---
+++ Data Types/src/Main.java
@@ -0,0 +1,22 @@
+
+public class Main {
+
+	public static void main(String[] args) {
+		int boy = 160;
+		double kilo = 47.5;
+		boolean liseyiTamamladimMi = true;
+		String favoriYemek = "Spagetti";
+		byte negatif = -128;
+		byte pozitif = 127;
+		char basHarfim = 'E';
+
+		System.out.println(boy);
+		System.out.println(kilo);
+		System.out.println(liseyiTamamladimMi);
+		System.out.println(favoriYemek);
+		System.out.println(negatif);
+		System.out.println(pozitif);
+		System.out.println(basHarfim);
+	}
+
+}
